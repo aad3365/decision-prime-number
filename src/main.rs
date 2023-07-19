b9f44c5606f3fc5b1b33f15627340aa2015e4802
aad3365 <aad336545@gmail.com>
2023-07-19 22:17:30 +0900

    divide to another function

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,6 @@
 use std::io;
 
-fn main() {
-    let mut line = String::new();
-
-    println!("please input some positive number");
-    io::stdin().read_line(&mut line).expect("something was wrong while getting input.");
-    
-    let number = line.trim().parse::<u32>().expect("input must be positive integer");
+fn decision_version1(number: &u32) -> bool {
     let mut divisors: Vec<u32> = Vec::new();
 
     for i in 1..number + 1 {
@@ -15,9 +9,20 @@ fn main() {
         }
     }
 
-    if divisors.len() > 2 {
-        println!("{} is not a prime number!", number);
-    } else {
-        println!("{} is prime number!", number);
+    divisors.len() == 2
+}
+
+fn main() {
+    let mut line = String::new();
+
+    println!("please input some positive number");
+    io::stdin().read_line(&mut line).expect("something was wrong while getting input.");
+    
+    let number = line.trim().parse::<u32>().expect("input must be positive integer");
+    assert!(number != 1, "number should be bigger than 1");
+    
+    match decision_version1(&number) {
+        true => println!("{} is prime number!", number),
+        false => println!("{} is not a prime number!", number)
     }
 }
